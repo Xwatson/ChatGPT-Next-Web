dd6777c496502ab91835075dadf3bf3eb5abf415
xwatson <xwatson@foxmail.com>
2024-05-21 15:22:14 +0800

    feat: site title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,12 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          XwatsonChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          欢迎使用
+          <br />
+          推荐模型 gpt-4-turbo-2024-04-09
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
